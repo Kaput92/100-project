d21ff0e04ee9ce2c2f5e4b3c45df0311cfec2014
Kaput92 <emanuele.caputo92@gmail.com>
2025-03-03 06:51:54 +0100

    Added project 30

---
+++ 30 - Form Wave/script.js
@@ -0,0 +1,10 @@
+const allLabels = document.querySelectorAll(".form-control label");
+
+allLabels.forEach((label) => {
+  label.innerHTML = label.innerHTML
+    .split("")
+    .map((letter, index) => {
+      return `<span style="transition-delay: ${index * 50}ms">${letter}</span>`;
+    })
+    .join("");
+});
