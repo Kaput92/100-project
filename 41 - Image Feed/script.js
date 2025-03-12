a26ae126534e33afb4075a0d56596951d4bc8fe8
Kaput92 <emanuele.caputo92@gmail.com>
2025-03-12 10:58:33 +0100

    added project 41 - Random Image Feed gen

---
+++ 41 - Image Feed/script.js
@@ -0,0 +1,20 @@
+const containerr = document.querySelector(".content");
+
+const baseURL = `https://picsum.photos/`;
+
+const rows = 7;
+
+for (let i = 0; i < rows * 3; i++) {
+  const img = document.createElement("img");
+  img.src = `${baseURL}${randomSize()}`;
+  containerr.appendChild(img);
+}
+
+
+function randomSize() {
+  return `${randomNumber()}/${randomNumber()}`;
+}
+
+function randomNumber() {
+  return Math.floor(Math.random() * 10) + 300;
+}
