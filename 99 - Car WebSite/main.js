b172de43816b09e6f52be0d555dd4d458bf9094d
Kaput92 <emanuele.caputo92@gmail.com>
2025-06-03 06:35:01 +0200

    background-header, about

--- 99 - Car WebSite/main.js
+++ 99 - Car WebSite/main.js
@@ -43,6 +43,14 @@ const swiperHome = new Swiper(".home__swiper", {
 });
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+const bgHeader = () =>{
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('bg-header') 
+                       : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', bgHeader)
+
 
 /*=============== SHOW SCROLL UP ===============*/
 
